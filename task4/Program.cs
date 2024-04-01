27e91adcb3b188729dd3ff5199f1210481ce2b40
Sayara <sayarabirova@gmail.com>
2024-04-01 19:52:35 +0600

    task4 added

---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿int num = new Random().Next(0,10000);
+Console.WriteLine(num);
+int res = 0;
+while (num >= 10)
+{
+    res = num % 10;
+    num/=10;
+    Console.WriteLine(res + ", ");
+}
+res = num % 10;
+Console.WriteLine(res);
