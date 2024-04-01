e91c615b1c8538542c44419c43a6749fb6e5f34d
Sayara <sayarabirova@gmail.com>
2024-04-01 19:35:18 +0600

    task2 added

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите координаты");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1");
+}
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("2");
+}
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("3");
+}
+if (x > 0 && y < 0)
+{
+    Console.WriteLine("4");
+}
