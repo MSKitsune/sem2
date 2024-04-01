7ca6da07af7a930384a871eaadb92331181ca087
Sayara <sayarabirova@gmail.com>
2024-04-01 19:35:29 +0600

    task3 added

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int res1 = 0;
+int res2 = 0;
+
+if (num < 10 && num > 99)
+{
+    Console.WriteLine("ошибка");
+}
+    res1 = num % 10;
+    res2 = num / 10;
+    if (res1 > res2)
+    {
+        Console.WriteLine(res1);
+    }
+    else
+    {
+        Console.WriteLine(res2);
+    }
